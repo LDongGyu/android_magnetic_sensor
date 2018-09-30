88e34faf8932c06968e99498d927d1a57720167a
LDongGyu <ehdrbchd@naver.com>
2018-10-01 01:04:38 +0900

    360도로 표현

--- app/src/main/java/com/example/leedonggyu/magnetic_sensor/MainActivity.java
+++ app/src/main/java/com/example/leedonggyu/magnetic_sensor/MainActivity.java
@@ -16,10 +16,25 @@ public class MainActivity extends AppCompatActivity {
     private SensorManager mySensorManager; // 센서 매니저
     private SensorEventListener magnetic_Listener; // 센서 리스너
     private Sensor myMagnetic; // 센서
+    private Sensor myAccele;
+
+    private float[] mGravity = new float[3];
+    private float[] mGeomagnetic = new float[3];
+    private float[] Rotation = new float[9];
+    private float[] I = new float[9];
+
+    private float azimuth;
+    private float azimuthFix;
 
     private TextView x_text;
     private TextView y_text;
     private TextView z_text;
+    private TextView max;
+    private TextView min;
+    private TextView correct;
+
+    private double max_dir;
+    private double min_dir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,22 +44,69 @@ public class MainActivity extends AppCompatActivity {
         x_text = (TextView)findViewById(R.id.x);
         y_text = (TextView)findViewById(R.id.y);
         z_text = (TextView)findViewById(R.id.z);
+        max = (TextView)findViewById(R.id.max);
+        min = (TextView)findViewById(R.id.min);
+        correct = (TextView)findViewById(R.id.correct);
+
+        max_dir = 0.0;
+        min_dir = 0.0;
 
         mySensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         // 자이로스코프 센서를 사용하겠다고 등록
         myMagnetic = mySensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        myAccele = mySensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
         magnetic_Listener = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent sensorEvent) {
-                double x = sensorEvent.values[0] * 10.0;
-                double y = sensorEvent.values[1] * 10.0;
-                double z = sensorEvent.values[2] * 10.0;
+                final float alpha = 0.97f;
 
-                x_text.setText("x"+String.format("%f",x));
-                y_text.setText("y"+String.format("%f",y));
-                z_text.setText("z"+String.format("%f",z));
+                synchronized (this) {
+                    if(sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+                        mGravity[0] = alpha * mGravity[0] + (1-alpha)*sensorEvent.values[0];
+                        mGravity[1] = alpha * mGravity[1] + (1-alpha)*sensorEvent.values[1];
+                        mGravity[2] = alpha * mGravity[2] + (1-alpha)*sensorEvent.values[2];
+                    }
+                    if(sensorEvent.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+                        mGeomagnetic[0] = alpha * mGeomagnetic[0] + (1 - alpha) * sensorEvent.values[0];
+                        mGeomagnetic[1] = alpha * mGeomagnetic[1] + (1 - alpha) * sensorEvent.values[1];
+                        mGeomagnetic[2] = alpha * mGeomagnetic[2] + (1 - alpha) * sensorEvent.values[2];
+                    }
+
+                    boolean success = SensorManager.getRotationMatrix(Rotation,I,mGravity,mGeomagnetic);
+
+                    if(success){
+                        float orientaion[] = new float[3];
+                        SensorManager.getOrientation(Rotation,orientaion);
+                        azimuth = (float)Math.toDegrees(orientaion[0]);
+                        azimuth = (azimuth + 360)%360;
+                    }
+                        double x = azimuth;
+                     /*   double y = sensorEvent.values[1];
+                        double z = sensorEvent.values[2];*/
+
+                        if (x > max_dir) {
+                            max_dir = x;
+                            max.setText("max" + String.format("%f", max_dir));
+                        }
+                        if (x < min_dir) {
+                            min_dir = x;
+                            min.setText("min" + String.format("%f", min_dir));
+                        }
+
+                        if(25 <= x && x <=30){
+                            correct.setText("건물 정보 출력");
+                        }
+                        else{
+                            correct.setText("신호 없음");
+                        }
+                        
+                        x_text.setText("x" + String.format("%f", x));
+                     /*   y_text.setText("y" + String.format("%f", y));
+                        z_text.setText("z" + String.format("%f", z));*/
+
+                }
             }
-
             @Override
             public void onAccuracyChanged(Sensor sensor, int i) {
 
@@ -55,6 +117,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         mySensorManager.registerListener(magnetic_Listener, myMagnetic,SensorManager.SENSOR_DELAY_UI);
+        mySensorManager.registerListener(magnetic_Listener, myAccele,SensorManager.SENSOR_DELAY_UI);
+
     }
     protected void onPause() {
         super.onPause();
@@ -64,4 +128,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
     }
+
+    public void setAzimuthFix(float fix) {
+        azimuthFix = fix;
+    }
+
+    public void resetAzimuthFix() {
+        setAzimuthFix(0);
+    }
+
 }
